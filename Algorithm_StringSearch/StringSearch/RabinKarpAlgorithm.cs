08027893594d1b289f7b1ff28d798f4e15cf61d3
gotoa1234 <cap8825@gmail.com>
2024-05-22 18:23:08 +0800

    1. add base example .

---
+++ Algorithm_StringSearch/StringSearch/RabinKarpAlgorithm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_StringSearch.StringSearch
+{
+    public class RabinKarpAlgorithmExecute
+    {
+        public void Execute()
+        {
+
+        }
+    }
+
+
+    public class RabinKarpAlgorithm
+    {
+        public RabinKarpAlgorithm()
+        {
+        }
+
+        public void CountingAsedingSort(List<int> inputItem)
+        {
+            int[] counting = new int[100];
+            for (int i = 0; i < inputItem.Count; i++)
+            {
+                counting[inputItem[i]]++;
+            }
+
+            for (int i = 0; i < counting.Length; i++)
+            {
+                if (counting[i] > 0)
+                {
+                    Console.WriteLine(i + " : " + counting[i]);
+                }
+            }
+        }
+    }
+}
