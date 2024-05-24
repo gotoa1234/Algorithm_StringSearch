442e7a0e85645fddae969136cb3e6476918e537f
gotoa1234 <cap8825@gmail.com>
2024-05-24 19:05:29 +0800

    1. 簽入代碼

--- Algorithm_StringSearch/StringSearch/RabinKarpAlgorithm.cs
+++ Algorithm_StringSearch/StringSearch/RabinKarpAlgorithm.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static Algorithm_StringSearch.StringSearch.RabinKarpAlgorithm;
 
 namespace Algorithm_StringSearch.StringSearch
 {
@@ -10,7 +6,11 @@ namespace Algorithm_StringSearch.StringSearch
     {
         public void Execute()
         {
-            var temp = new RabinKarpAlgorithm();
+            // ASCII 中 A=65, F=70, K=75
+            string text = "AAAFFFKKK";
+            string pattern = "AFF";
+            new Method_RollingHash(text, pattern);
+            new Method_RabinFingerprint(text, pattern);
         }
     }
 
@@ -19,98 +19,191 @@ namespace Algorithm_StringSearch.StringSearch
     {
         public RabinKarpAlgorithm()
         {
-            string text = "ABCCDDAEFG";
-            string pattern = "CDD";
-            int prime = 101; // A prime number
-
-            int result = RabinKarpSearch(text, pattern, prime);
-
-            if (result != -1)
-            {
-                Console.WriteLine($"Pattern found at index {result}");
-            }
-            else
-            {
-                Console.WriteLine("Pattern not found");
-            }
         }
 
-        public void CountingAsedingSort(List<int> inputItem)
+        /// <summary>
+        /// 第一種：旋轉哈希的實現
+        /// </summary>
+        public class Method_RollingHash
         {
-            int[] counting = new int[100];
-            for (int i = 0; i < inputItem.Count; i++)
-            {
-                counting[inputItem[i]]++;
-            }
+            /* 演算:
+             * Step1 : 得到 pattern 65+70+70 = 205 的 Hash 值 (AFF)， 並且得到 text 三個字元 65+65+65 = 195 的 Hash 值 (AAA)
+               Step2 : 進行滑動窗口處理，如果 Hash 相等，則進一步比較，如果不相等，則更新 Hash 值
+               Step3 : 更新 text Hash值 ，從 195 變成 65+65+70 = 200 (AAA -> AAF)
+               Step4 : 進行滑動窗口處理，如果 Hash 相等，則進一步比較，如果不相等，則更新 Hash 值
+               Step5 : 更新 text Hash值 ，從 200 變成 65+70+70 = 205 (AAF -> AFF)
+               Step6 : 進行滑動窗口處理，此時 Hash 相等，進一步比較，完全匹配，返回索引
+             */
 
-            for (int i = 0; i < counting.Length; i++)
+            /// <summary>
+            /// 建構式
+            /// </summary>
+            /// <param name="text">原始資料</param>
+            /// <param name="pattern">查詢的字串</param>
+            public Method_RollingHash(string text, string pattern)
             {
-                if (counting[i] > 0)
+                int result = RabinKarpSearch(text, pattern);
+
+                if (result != -1)
                 {
-                    Console.WriteLine(i + " : " + counting[i]);
+                    Console.WriteLine($"Pattern found at index {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Pattern not found");
                 }
             }
-        }
 
-        public static int RabinKarpSearch(string text, string pattern, int prime)
-        {
-            int n = text.Length;
-            int m = pattern.Length;
-            int i, j;
-            int p = 0; // pattern hash value
-            int t = 0; // text hash value
-            int h = 1;
-            int d = 256; // Number of characters in the input alphabet
-
-            // Calculate the value of h (h = d^(m-1) % prime)
-            for (i = 0; i < m - 1; i++)
+            /// <summary>
+            /// 拉賓卡普搜索算法 - 旋轉哈希 (Rabin-Karp Algorithm)
+            /// </summary>
+            /// <param name="text">原始資料</param>
+            /// <param name="pattern">查詢的字串</param>
+            /// <returns></returns>
+            private int RabinKarpSearch(string text, string pattern)
             {
-                h = (h * d) % prime;
-            }
+                // 1. 預處理：計算文字和查詢對象的 Hash
+                int textLength = text.Length;
+                int targetLength = pattern.Length;
+                int patternHash = CalculateRollingHash(pattern, 0, targetLength);
+                int textHash = CalculateRollingHash(text, 0, targetLength);
 
-            // Calculate the hash value of the pattern and first window of text
-            for (i = 0; i < m; i++)
-            {
-                p = (d * p + pattern[i]) % prime;
-                t = (d * t + text[i]) % prime;
-            }
-
-            // Slide the pattern over text one by one
-            for (i = 0; i <= n - m; i++)
-            {
-                // Check the hash values of the current window of text and pattern
-                if (p == t)
+                // 2. 滑動窗口處理 ※最多滑動 textLength - targetLength 次
+                for (int index = 0; index <= textLength - targetLength; index++)
                 {
-                    // If the hash values match, then only check characters one by one
-                    for (j = 0; j < m; j++)
+                    // 3-1. 匹配，如果 Hash 相等，則進一步比較
+                    if (patternHash == textHash)
                     {
-                        if (text[i + j] != pattern[j])
+                        int findIndex = 0;
+                        // 3-2. 進一步比較，比對字元是否相等
+                        // ※旋轉哈希有可能碰撞，所以需要進一步比對 EX: ABCD 與 DCBA 相同 Hash
+                        for (; findIndex < targetLength; findIndex++)
                         {
-                            break;
+                            if (text[index + findIndex] != pattern[findIndex])
+                                break;
                         }
+                        // 4. 如果完全匹配，返回索引
+                        if (findIndex == targetLength)
+                            return index;
                     }
-
-                    // If p == t and pattern[0...m-1] == text[i...i+m-1]
-                    if (j == m)
+                    // 3-3. 不匹配，更新 Hash 值
+                    if (index < textLength - targetLength)
                     {
-                        return i; // Pattern found at index i
+                        textHash = UpdateRollingHash(textHash, text[index], text[index + targetLength]);
                     }
                 }
+                // 5. 沒找到，返回 -1
+                return -1;
+            }
 
-                // Calculate hash value for next window of text
-                if (i < n - m)
+            /// <summary>
+            /// 計算 Hash 值 (累加字符的 ASCII 值)
+            /// </summary>
+            /// <param name="str">傳入的字串</param>
+            /// <param name="start">起始索引</param>
+            /// <param name="length">總長度</param>
+            /// <returns></returns>
+            private int CalculateRollingHash(string str, int start, int length)
+            {
+                int hash = 0;
+                for (int index = start; index < start + length; index++)
                 {
-                    t = (d * (t - text[i] * h) + text[i + m]) % prime;
+                    hash += str[index]; 
+                }
+                return hash;
+            }
 
-                    // We might get negative value of t, converting it to positive
-                    if (t < 0)
-                    {
-                        t = (t + prime);
-                    }
+            /// <summary>
+            /// 沒找到時進入 - 更新旋轉 Hash 值 
+            /// <para>※只有一個字元的變動</para>
+            /// </summary>    
+            /// <param name="oldHash">當前舊有的 Hash 值</param>
+            /// <param name="oldChar">舊的單一字元</param>
+            /// <param name="newChar">新的單一字元</param>
+            /// <returns></returns>
+            private int UpdateRollingHash(int oldHash, char oldChar, char newChar)
+            {
+                return oldHash - oldChar + newChar;
+            }
+        }
+
+        /// <summary>
+        /// 第二種：拉賓指紋的實現
+        /// </summary>
+        public class Method_RabinFingerprint
+        {
+            /* 演算:
+ * Step1 : 得到 pattern 65+70+70 = 205 的 Hash 值 (AFF)， 並且得到 text 三個字元 65+65+65 = 195 的 Hash 值 (AAA)
+   Step2 : 進行滑動窗口處理，如果 Hash 相等，則進一步比較，如果不相等，則更新 Hash 值
+   Step3 : 更新 text Hash值 ，從 195 變成 65+65+70 = 200 (AAA -> AAF)
+   Step4 : 進行滑動窗口處理，如果 Hash 相等，則進一步比較，如果不相等，則更新 Hash 值
+   Step5 : 更新 text Hash值 ，從 200 變成 65+70+70 = 205 (AAF -> AFF)
+   Step6 : 進行滑動窗口處理，此時 Hash 相等，進一步比較，完全匹配，返回索引
+ */
+
+            /// <summary>
+            /// 建構式
+            /// </summary>
+            /// <param name="text">原始資料</param>
+            /// <param name="pattern">查詢的字串</param>
+            public Method_RabinFingerprint(string text, string pattern)
+            {
+                // 使用質數 prime 進行模運算
+                int prime = 101; 
+
+                int result = RabinKarpSearch(text, pattern, prime);
+
+                if (result != -1)
+                {
+                    Console.WriteLine($"Pattern found at index {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Pattern not found");
                 }
             }
 
-            return -1; // Pattern not found
+            private int CalculateRabinFingerprint(string str, int start, int length, int prime)
+            {
+                int hash = 0;
+                for (int i = start; i < start + length; i++)
+                {
+                    hash = (hash * 256 + str[i]) % prime; // 使用质数 prime 进行模运算
+                }
+                return hash;
+            }
+
+            private int RabinKarpSearch(string text, string pattern, int prime)
+            {
+                int n = text.Length;
+                int m = pattern.Length;
+                int patternHash = CalculateRabinFingerprint(pattern, 0, m, prime);
+                int textHash = CalculateRabinFingerprint(text, 0, m, prime);
+
+                for (int i = 0; i <= n - m; i++)
+                {
+                    if (patternHash == textHash)
+                    {
+                        int j;
+                        for (j = 0; j < m; j++)
+                        {
+                            if (text[i + j] != pattern[j])
+                                break;
+                        }
+                        if (j == m)
+                            return i;
+                    }
+                    if (i < n - m)
+                    {
+                        // 更新文本窗口的哈希值
+                        textHash = (textHash * 256 + text[i + m] - text[i] * (int)Math.Pow(256, m)) % prime;
+                        // 负值转正
+                        if (textHash < 0)
+                            textHash = (textHash + prime);
+                    }
+                }
+                return -1;
+            }
         }
     }
 }
