fe5ddbab96c3617892c58ed5f242648b74c88c91
gotoa1234 <cap8825@gmail.com>
2024-07-02 15:31:30 +0800

    1. fix code.

--- Algorithm_StringSearch/Program.cs
+++ Algorithm_StringSearch/Program.cs
@@ -1,13 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Algorithm_StringSearch.StringSearch;
 
+Console.WriteLine("====");
+
 // 1. Rabin-Karp Algorithm
 var alRabinKarp = new RabinKarpAlgorithmExecute();
 alRabinKarp.Execute();
 
+Console.WriteLine("====");
+
 // 2. KMP Algorithm
 var alKMP = new KMPAlgorithmExecute();
 alKMP.Execute();
+
+Console.WriteLine("====");
 
 // 3. Boyer Moore Algorithm
 var alBoyerMoore = new BoyerMooreAlgorithmExecute();
--- Algorithm_StringSearch/StringSearch/BoyerMooreAlgorithm.cs
+++ Algorithm_StringSearch/StringSearch/BoyerMooreAlgorithm.cs
@@ -4,12 +4,13 @@
     {
         public void Execute()
         {
-            var method = new BoyerMooreAlgorithm();
-            string target = "abacaabaccabacabacab";
-            string pattern = "abacab";
-            
-            Console.WriteLine("匹配位置:");
-            //method.BoyerMooreSearch(target, pattern);
+            string text = "HERE IS A SIMPLE EXAMPLE";
+            string pattern = "EXAMPLE";
+            Console.WriteLine($@"原始文本：{text}");
+            Console.WriteLine($@"搜尋字串：{pattern}");
+            new BoyerMooreAlgorithm.BadCharacterExample(text, pattern);
+            new BoyerMooreAlgorithm.GoodSuffixExample(text, pattern);
+            new BoyerMooreAlgorithm.BoyerMooreExample(text, pattern);
         }
     }
 
@@ -17,12 +18,9 @@
     /// 
     /// </summary>
     public class BoyerMooreAlgorithm
-    {
-        private readonly int ALPHABET_SIZE = 256;
-        
+    {        
         public BoyerMooreAlgorithm()
         {
-            
         }
 
         /// <summary>
@@ -30,62 +28,56 @@
         /// </summary>
         public class BadCharacterExample
         {
-            private int[] badCharacterTable;
-            private readonly int ALPHABET_SIZE = 256;
-            private string pattern;
             public BadCharacterExample(string text, string pattern)
             {
-                this.pattern = pattern;
-                badCharacterTable = BuildBadCharacterTable(pattern);
+                Console.WriteLine("1. 第一種方式：壞字符表");
+                Search(text, pattern);
+                Console.WriteLine();
+                Console.WriteLine();
             }
 
-            private int[] BuildBadCharacterTable(string pattern)
+            /// <summary>
+            /// 1-3. 建立壞字元表
+            /// </summary>            
+            private Dictionary<char, int> BadCharHeuristic(string pattern)
+            {
+                var result= new Dictionary<char, int>();
+                for (int index = 0; index < pattern.Length; index++)
+                    result[pattern[index]] = index;
+                return result;
+            }
+
+            /// <summary>
+            /// 1-2. 執行演算法 - 搜尋指定字串
+            /// </summary>           
+            private void Search(string text, string pattern)
             {
                 int m = pattern.Length;
-                int[] table = new int[ALPHABET_SIZE];
-
-                for (int i = 0; i < ALPHABET_SIZE; i++)
-                    table[i] = -1;
-
-                for (int i = 0; i < m; i++)
-                    table[(int)pattern[i]] = i;
-
-                return table;
-            }
-
-            public int Search(string text)
-            {
                 int n = text.Length;
-                int m = pattern.Length;
-                int s = 0; // s is the shift of the pattern with respect to text
 
-                while (s <= (n - m))
+                //1-3. 找出壞字元表
+                var badchar = BadCharHeuristic(pattern);
+
+                //1-4. 開始搜尋
+                int currentIndex = 0;// 起始位置
+                while (currentIndex <= (n - m))// 不可超過最大長度
                 {
-                    int j = m - 1;
+                    //1-5. 從後往前比對
+                    int moveIndex = m - 1;
 
-                    while (j >= 0 && pattern[j] == text[s + j])
-                        j--;
+                    // 1-6. 若有不同，則移動到壞字元表的位置
+                    while (moveIndex >= 0 && pattern[moveIndex] == text[currentIndex + moveIndex])
+                        moveIndex--;
 
-                    if (j < 0)
+                    if (moveIndex == -1)// 1-7. 若比對到最後，則表示找到
                     {
-                        return s; // Pattern found at index s
-                        s += (s + m < n) ? m - badCharacterTable[text[s + m]] : 1;
+                        Console.WriteLine("Pattern occurs at index " + currentIndex);
+                        currentIndex += (currentIndex + m < n) ? m - badchar.GetValueOrDefault(text[currentIndex + m], -1) : 1;
                     }
-                    else
+                    else// 1-8. 若有不同，則移動到壞字元表的位置
                     {
-                        s += Math.Max(1, j - badCharacterTable[text[s + j]]);
+                        currentIndex += Math.Max(1, moveIndex - badchar.GetValueOrDefault(text[currentIndex + moveIndex], -1));
                     }
-                }
-
-                return -1; // Pattern not found
-            }
-
-            public void PrintBadCharacterTable()
-            {
-                for (int i = 0; i < ALPHABET_SIZE; i++)
-                {
-                    if (badCharacterTable[i] != -1)
-                        Console.WriteLine($"Char {((char)i)}: {badCharacterTable[i]}");
                 }
             }
         }
@@ -95,15 +87,160 @@
         /// </summary>
         public class GoodSuffixExample
         {
+            public GoodSuffixExample(string text, string pattern)
+            {
+                Console.WriteLine("2. 第二種方式：好後綴表");
+                Search(text, pattern);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
 
+            private void PreprocessStrongSuffix(int[] shift, int[] bpos, string patten)
+            {
+                int pattenLength = patten.Length;
+                int index = pattenLength;
+                int j = pattenLength + 1;
+                bpos[index] = j;
+                while (index > 0)
+                {
+                    while (j <= pattenLength && patten[index - 1] != patten[j - 1])
+                    {
+                        if (shift[j] == 0)
+                            shift[j] = j - index;
+                        j = bpos[j];
+                    }
+                    index--; j--;
+                    bpos[index] = j;
+                }
+            }
+
+            private void PreprocessCase2(int[] shift, int[] bpos, int m)
+            {
+                int i, j;
+                j = bpos[0];
+                for (i = 0; i <= m; i++)
+                {
+                    if (shift[i] == 0)
+                        shift[i] = j;
+                    if (i == j)
+                        j = bpos[j];
+                }
+            }
+
+            private void Search(string text, string pattern)
+            {
+                int currentIndex = 0, moveIndex = 0;
+                int m = pattern.Length;
+                int n = text.Length;
+
+                int[] bpos = new int[m + 1];
+                int[] shift = new int[m + 1];
+
+                for (int i = 0; i < m + 1; i++) shift[i] = 0;
+
+                PreprocessStrongSuffix(shift, bpos, pattern, m);
+                PreprocessCase2(shift, bpos, m);
+
+                while (currentIndex <= n - m)
+                {
+                    moveIndex = m - 1;
+
+                    while (moveIndex >= 0 && pattern[moveIndex] == text[currentIndex + moveIndex])
+                        moveIndex--;
+
+                    if (moveIndex < 0)
+                    {
+                        Console.WriteLine("Pattern occurs at index " + currentIndex);
+                        currentIndex += shift[0];
+                    }
+                    else
+                        currentIndex += shift[moveIndex + 1];
+                }
+            }
         }
 
         /// <summary>
-        /// 3. 第三種方式：混合法
+        /// 3. 第三種方式：混合法 (完整最基本版本的 Boyer-Moore)
         /// </summary>
         public class BoyerMooreExample
         {
+            public BoyerMooreExample(string text, string pattern)
+            {
+                Console.WriteLine("2. 第三種方式：混合法 (完整最基本版本的 Boyer-Moore)");
+                Search(text, pattern);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
 
+            private Dictionary<char, int> BadCharHeuristic(string pattern)
+            {
+                var result = new Dictionary<char, int>();
+                for (int index = 0; index < pattern.Length; index++)
+                    result[pattern[index]] = index;
+                return result;
+            }
+
+            private void PreprocessStrongSuffix(int[] shift, int[] bpos, string pat, int m)
+            {
+                int i = m, j = m + 1;
+                bpos[i] = j;
+                while (i > 0)
+                {
+                    while (j <= m && pat[i - 1] != pat[j - 1])
+                    {
+                        if (shift[j] == 0)
+                            shift[j] = j - i;
+                        j = bpos[j];
+                    }
+                    i--; j--;
+                    bpos[i] = j;
+                }
+            }
+
+            private void PreprocessCase2(int[] shift, int[] bpos, int m)
+            {
+                int i, j;
+                j = bpos[0];
+                for (i = 0; i <= m; i++)
+                {
+                    if (shift[i] == 0)
+                        shift[i] = j;
+                    if (i == j)
+                        j = bpos[j];
+                }
+            }
+
+            public void Search(string text, string pattern)
+            {
+                int m = pattern.Length;
+                int n = text.Length;
+                
+                int[] bpos = new int[m + 1];
+                int[] shift = new int[m + 1];
+
+                for (int i = 0; i < m + 1; i++) shift[i] = 0;
+
+                var badchar = BadCharHeuristic(pattern);
+                PreprocessStrongSuffix(shift, bpos, pattern, m);
+                PreprocessCase2(shift, bpos, m);
+
+                int s = 0;
+                while (s <= (n - m))
+                {
+                    int j = m - 1;
+
+                    while (j >= 0 && pattern[j] == text[s + j])
+                        j--;
+
+                    if (j < 0)
+                    {
+                        Console.WriteLine("Pattern occurs at index " + s);
+                        s += shift[0];
+                    }
+                    else
+                        s += Math.Max(shift[j + 1], j - badchar.GetValueOrDefault(text[s + j], -1));
+                }
+            }
         }
     }
 }
